2a9df80bd77307794959eb2d3b3f56bcdafd1ff2
Ales Justin <ales.justin@gmail.com>
2021-12-16 15:13:37 +0100

    Reactive fixes validation.

--- common/src/main/java/com/alesj/qcl/common/rest/ValidatorInterceptor.java
+++ common/src/main/java/com/alesj/qcl/common/rest/ValidatorInterceptor.java
@@ -7,9 +7,7 @@ import javax.interceptor.InvocationContext;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
-import java.lang.reflect.Executable;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import javax.validation.executable.ExecutableValidator;
 import java.util.Set;
 
 /**
@@ -19,40 +17,18 @@ import java.util.Set;
 @Interceptor
 public class ValidatorInterceptor {
 
-    private static Method validate;
-
     @Inject
     Validator validator;
 
     @AroundInvoke
     public Object validate(InvocationContext context) throws Throwable {
-        Set<ConstraintViolation<Object>> violations = getConstraintViolations(context);
+        ExecutableValidator ev = validator.forExecutables();
+        Set<ConstraintViolation<Object>> violations = ev.validateParameters(
+            context.getTarget(), context.getMethod(), context.getParameters()
+        );
         if (violations.size() > 0) {
             throw new ConstraintViolationException(violations);
         }
         return context.proceed();
-    }
-
-    private static synchronized Method getValidate() throws Throwable {
-        if (validate == null) {
-            Class<?> clazz = Class.forName("org.hibernate.validator.internal.engine.ValidatorImpl");
-            validate = clazz.getDeclaredMethod(
-                "validateParameters",
-                Object.class, Executable.class, Object[].class, Class[].class);
-            validate.setAccessible(true);
-        }
-        return validate;
-    }
-
-    @SuppressWarnings("unchecked")
-    private Set<ConstraintViolation<Object>> getConstraintViolations(InvocationContext context) throws Throwable {
-        try {
-            return (Set<ConstraintViolation<Object>>) getValidate().invoke(
-                validator,
-                null, context.getMethod(), context.getParameters(), new Class[0]
-            );
-        } catch (InvocationTargetException e) {
-            throw e.getCause();
-        }
     }
 }
