a0eb4ab261417cd0cf62ce3bff2085a01dd577ff
Ales Justin <ales.justin@gmail.com>
2022-02-23 11:37:39 +0100

    Test OTel deps.

--- app/src/main/java/com/alesj/qcl/app/Configuration.java
+++ app/src/main/java/com/alesj/qcl/app/Configuration.java
@@ -1,15 +1,11 @@
 package com.alesj.qcl.app;
 
-import com.alesj.qcl.common.rest.Input;
 import io.quarkus.runtime.StartupEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import javax.enterprise.inject.Disposes;
-import javax.inject.Named;
-import javax.inject.Singleton;
 
 /**
  * @author Ales Justin
@@ -18,24 +14,10 @@ import javax.inject.Singleton;
 public class Configuration {
     private static final Logger log = LoggerFactory.getLogger(Configuration.class);
 
-    @Singleton
-    @Named("myInput")
-    public Input input() {
-        return new Input();
-    }
-
     public void startInput(
-        @Observes StartupEvent evt,
-        @Named("myInput") Input input
+        @Observes StartupEvent evt
     ) {
         log.info("Started input...");
-    }
-
-    public void stopInput(
-        @Disposes @Named("myInput") AutoCloseable input
-    ) throws Exception {
-        log.info("Stopping input...");
-        input.close();
     }
 
 }
