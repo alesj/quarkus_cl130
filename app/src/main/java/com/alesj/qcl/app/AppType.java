fd85985546c02f5f1c592b8cdb372999e4ccb0bb
Ales Justin <ales.justin@gmail.com>
2022-11-02 10:17:25 +0100

    Add xDS client

---
+++ app/src/main/java/com/alesj/qcl/app/AppType.java
@@ -0,0 +1,9 @@
+package com.alesj.qcl.app;
+
+/**
+ * @author Ales Justin
+ */
+public enum AppType {
+    SERVER,
+    CLIENT
+}
--- app/src/main/java/com/alesj/qcl/app/Configuration.java
+++ app/src/main/java/com/alesj/qcl/app/Configuration.java
@@ -1,10 +1,20 @@
 package com.alesj.qcl.app;
 
+import examples.GreeterGrpc;
+import examples.HelloReply;
+import examples.HelloRequest;
+import io.grpc.ChannelCredentials;
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
 import io.grpc.InsecureServerCredentials;
+import io.grpc.ManagedChannel;
 import io.grpc.Server;
 import io.grpc.ServerCredentials;
+import io.grpc.StatusRuntimeException;
 import io.grpc.protobuf.services.ProtoReflectionService;
+import io.grpc.xds.XdsChannelCredentials;
 import io.grpc.xds.XdsServerBuilder;
+import io.grpc.xds.XdsServerCredentials;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
@@ -14,6 +24,9 @@ import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -26,12 +39,73 @@ public class Configuration {
     @ConfigProperty(name = "xds.port", defaultValue = "30051")
     int port;
 
+    @ConfigProperty(name = "xds.type", defaultValue = "SERVER")
+    AppType type;
+
+    @ConfigProperty(name = "xds.target", defaultValue = "localhost:30051")
+    String target;
+
+    @ConfigProperty(name = "xds.user", defaultValue = "Ales")
+    String user;
+
+    @ConfigProperty(name = "xds.secure", defaultValue = "false")
+    boolean secure;
+
     private Server server;
 
-    public void startServer(@Observes StartupEvent evt) throws Exception {
+    private ManagedChannel channel;
+    private GreeterGrpc.GreeterBlockingStub stub;
+    private ScheduledExecutorService service;
+
+    public void start(@Observes StartupEvent evt) {
+        log.info("App type: {}, secure: {}", type, secure);
+        switch (type) {
+            case SERVER:
+                startServer();
+                return;
+            case CLIENT:
+                startClient();
+        }
+    }
+
+    public void stop(@Observes ShutdownEvent evt) throws Exception {
+        switch (type) {
+            case SERVER:
+                stopServer();
+                return;
+            case CLIENT:
+                stopClient();
+        }
+    }
+
+    private void startClient() {
+        log.info("Starting xDS client -> {}", target);
+        ChannelCredentials credentials = InsecureChannelCredentials.create();
+        if (secure) {
+            credentials = XdsChannelCredentials.create(credentials);
+        }
+        channel = Grpc.newChannelBuilder(target, credentials).build();
+        stub = GreeterGrpc.newBlockingStub(channel);
+        service = new ScheduledThreadPoolExecutor(2);
+        service.scheduleAtFixedRate(this::greet, 5, 15, TimeUnit.SECONDS);
+    }
+
+    private void stopClient() throws Exception {
+        if (service != null) {
+            service.shutdown();
+        }
+        if (channel != null) {
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+
+    private void startServer() {
         new Thread(() -> {
             try {
                 ServerCredentials credentials = InsecureServerCredentials.create();
+                if (secure) {
+                    credentials = XdsServerCredentials.create(credentials);
+                }
                 server = XdsServerBuilder.forPort(port, credentials)
                     .addService(new HelloService())
                     .addService(ProtoReflectionService.newInstance()) // convenient for command line tools
@@ -46,7 +120,7 @@ public class Configuration {
         }, "xDS Server").start();
     }
 
-    public void stopServer(@Observes ShutdownEvent evt) {
+    private void stopServer() {
         if (server == null) return;
 
         // Start graceful shutdown
@@ -63,6 +137,21 @@ public class Configuration {
         } catch (InterruptedException ex) {
             server.shutdownNow();
         }
+    }
+
+    /** Say hello to server. */
+    private void greet() {
+        log.info("Will try to greet " + user + " ...");
+        String msg = user + "::" + ThreadLocalRandom.current().nextInt(100);
+        HelloRequest request = HelloRequest.newBuilder().setName(msg).build();
+        HelloReply response;
+        try {
+            response = stub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+            log.warn("RPC failed: {}", e.getStatus());
+            return;
+        }
+        log.info("Greeting: " + response.getMessage());
     }
 }
 
